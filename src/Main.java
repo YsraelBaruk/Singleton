a40dfb052f80c62e81fa0b041e065d2fc6da6c3e
israekbaruque992 <israekbaruque992@gmail.com>
2025-02-28 01:06:18 +0000

    thread unica

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import model.Singleton;
+
 public class Main {
     public static void main(String[] args) {
-
+        Singleton singleton = Singleton.getInstance("Hello, Singleton!");    
+        Singleton otherSingleton = Singleton.getInstance("Other Singleton, Hi!");
+        
+        System.out.println(singleton.value);
+        System.out.println(otherSingleton.value);
     }
 }

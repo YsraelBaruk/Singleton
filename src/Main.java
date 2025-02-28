cf6ce5de1823fc935fb3f1106f6082bd066e340b
israekbaruque992 <israekbaruque992@gmail.com>
2025-02-28 01:14:34 +0000

    multiThreaded

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import model.Singleton;
+
 public class Main {
     public static void main(String[] args) {
+        Thread threadCar = new Thread(new ThreadCar()); 
+        Thread threadMotor = new Thread(new ThreadMotor()); 
+        
+        threadMotor.start();
+        threadCar.start();
+    }
 
+    static class ThreadCar implements Runnable {
+        @Override
+        public void run(){
+            Singleton singleton = Singleton.getInstance("Car");
+            System.out.println(singleton.value);
+        }
+    }
+
+    static class ThreadMotor implements Runnable {
+        @Override
+        public void run(){
+            Singleton singleton = Singleton.getInstance("Motorcycle");
+            System.out.println(singleton.value);
+        }
     }
 }

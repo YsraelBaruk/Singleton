354556963a08cf598a9c6012a0a345c8f89eb298
israekbaruque992 <israekbaruque992@gmail.com>
2025-02-28 01:22:37 +0000

    threads seguras

--- src/model/Singleton.java
+++ src/model/Singleton.java
@@ -1,23 +1,24 @@
 package model;
 
 public final class Singleton {
-    
-    private static Singleton instance;
+    private static volatile Singleton instance;
+
     public String value;
 
     private Singleton(String value) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         this.value = value;
     }
 
     public static Singleton getInstance(String value) {
-        if (instance == null) {
-            instance = new Singleton(value);
+        Singleton result = instance;
+        if (result != null) {
+            return result;
         }
-        return instance;
+        synchronized(Singleton.class) {
+            if (instance == null) {
+                instance = new Singleton(value);
+            }
+            return instance;
+        }
     }
 }
